73dd60f87cc4b8a40a89a7c240a296c935612947
tori-parmz <119701826+tori-parmz@users.noreply.github.com>
2022-12-06 19:35:06 -0600

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 console.log("it works!")
 
+var myName = "Tori"
+let lastName = "Parham"
+const middleName = "deForest"
+console.log (myName)
+
+// myName = "TernPermz"
+
+// console.log (myName)
+// This is a comment
+
+/*
+This is a super long comment that no one will ever see.
+The rats absolutely will overrun this city.
+They will take over and skitter toward their freedom.
+All we have is time.
+Time to wait and time to wonder.
+About the future that awaits us.
+A future ruled by rats.
+
+*/
+console.log(myName + " " + middleName + " " + lastName)
+
 
